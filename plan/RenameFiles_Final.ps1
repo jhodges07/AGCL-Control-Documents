dad41df0b0f3858bbbfad0d408fa213c21bbeca6
jhodges07 <75496209+jhodges07@users.noreply.github.com>
2026-05-03 12:06:24 -0500

    Final naming standard

---
+++ plan/RenameFiles_Final.ps1
@@ -0,0 +1,14 @@
+﻿Get-ChildItem -File -Filter "*.md" | ForEach-Object {
+    $name = $_.Name.ToLower()
+
+    $name = $name `
+        -replace 'point_', '' `
+        -replace '__+', '_' `
+        -replace '—', '' `
+        -replace '-', '_' `
+        -replace '&', 'and' `
+        -replace '[^a-z0-9_\.]', '' `
+        -replace '_+', '_'
+
+    Rename-Item $_.FullName $name
+}
